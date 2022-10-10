1d7d2be1c3042d8ac709893815557072b7f56076
Pauline Feytel <pauline.feytel@gmail.com>
2022-10-10 21:54:10 +0200

    Add Title component

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Title from './Components/Title';
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Title />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Title/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Title = () => {
+    return <h1>Mes plantes</h1>
+};
+
+export default Title;
